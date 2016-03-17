d0a23eb969bd95c4e225d1e700faabb03ae94117
dudustefanello <eduardostefanello3@gmail.com>
2016-03-16 22:55:43 -0300

    Update EduardoStefanelloT2.v
    Código atualizado com o que rodou na placa

--- EduardoStefanelloT2.v
+++ EduardoStefanelloT2.v
@@ -1,36 +1,38 @@
-module values(input clk, output light);
+module teste01(input CLOCK_50,
+					output [7:0] LEDG,
+					output [9:0] LEDR);
 
-	reg [3:0] counter = 0;
+	reg [32:0] counter = 0;
 	reg lighton = 0;
 
-	assign light = lighton;
+	assign LEDG[7] = lighton;
+	assign LEDG[6] = lighton;
+	assign LEDG[5] = lighton;
+	assign LEDG[4] = lighton;
+	assign LEDG[3] = lighton;
+	assign LEDG[2] = lighton;
+	assign LEDG[1] = lighton;
+	assign LEDG[0] = lighton;
+	assign LEDR[9] = ~lighton;
+	assign LEDR[8] = ~lighton;
+	assign LEDR[7] = ~lighton;
+	assign LEDR[6] = ~lighton;
+	assign LEDR[5] = ~lighton;
+	assign LEDR[4] = ~lighton;
+	assign LEDR[3] = ~lighton;
+	assign LEDR[2] = ~lighton;
+	assign LEDR[1] = ~lighton;
+	assign LEDR[0] = ~lighton;
 
-	always @(posedge clk) begin
-		if(counter == 8) begin
+	always @(posedge CLOCK_50) begin
+		if(counter == 50000000) begin
 			lighton <= ~lighton;
 			counter <= 0;
 		end
 		else begin
-			counter <= counter + 1;
+			counter <= counter+1;
 		end
 	end
 
 endmodule
 
-module test;
-
-	reg clk;
-	wire light;
-
-	values adder(clk, light);
-
-	always #1 clk = ~clk;
-
-	initial begin
-		$dumpvars(0, adder);
-		clk <= 0;
-		#200;
-		$finish;
-	end
-
-endmodule
